b7ed988b688e3c16343bac306117be009f47005f
parasar99 <parasar.edu@gmail.com>
2016-06-12 22:51:33 -0400

    'minor'

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// configure a public directory to host static content
+
 app.use(express.static(__dirname + '/public'));
 
-// require ("./test/app.js")(app);
+
 require ("./assignment/app.js")(app);
-//require ("./project/bookshelf.app.js")(app);
+
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3007;
