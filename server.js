2b7246cbd02bfc796976f0be841e75d77ebed06e
parasar99 <parasar.edu@gmail.com>
2016-06-24 15:44:16 -0400

    'code_clean_up'

--- server.js
+++ server.js
@@ -121,7 +121,8 @@ var auth = function (req, res, next) {
 var ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-//require("./public/assignment/server/app.js")(app, mongoose, db);
+ 
 require("./public/project/server/app.js")(app, mongoose, db, auth);
+
 
 app.listen(port,ip);
