3b536cd734191eb8ac53ef8dd56845dce82877ab
parasar99 <parasar.edu@gmail.com>
2016-06-24 15:47:58 -0400

    'comenting_app.js'

--- assignment/app.js
+++ assignment/app.js
@@ -1,10 +1,10 @@
-module.exports = function(app) {
-
-    var models = require("./models/models.server.js")();
-
-    require("./services/user.service.server.js")(app, models);
-    require("./services/website.service.server.js")(app, models);
-    require("./services/page.service.server.js")(app, models);
-    require("./services/widget.service.server.js")(app, models);
-
-};
+// module.exports = function(app) {
+//
+//     var models = require("./models/models.server.js")();
+//
+//     require("./services/user.service.server.js")(app, models);
+//     require("./services/website.service.server.js")(app, models);
+//     require("./services/page.service.server.js")(app, models);
+//     require("./services/widget.service.server.js")(app, models);
+//
+// };
